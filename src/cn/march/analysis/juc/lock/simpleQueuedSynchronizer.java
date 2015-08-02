f1f20050f5f2db3ba7f58179eb220d8c2760dbd6
xiaohuishu <shuxiaohui@foxmail.com>
2015-08-02 14:56:39 +0800

    add cn.march.analysis.lock.simpleQueuedSynchronized.java

---
+++ src/cn/march/analysis/juc/lock/simpleQueuedSynchronizer.java
@@ -0,0 +1,24 @@
+
+
+
+	package cn.march.analysis.juc.lock;
+	
+	
+	
+	import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+	
+	
+	
+	public class simpleQueuedSynchronizer extends AbstractQueuedSynchronizer{
+	
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+	
+		
+		
+		
+		
+		
+	}
