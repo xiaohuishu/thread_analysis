85ca1b3bd17fb5bef52d2e4273e8833f8d5194b4
xiaohuishu <shuxiaohui@foxmail.com>
2015-08-10 15:18:17 +0800

    update cn.march.analysis.model.akka

--- src/cn/march/analysis/model/akka/LookupActor.java
+++ src/cn/march/analysis/model/akka/LookupActor.java
@@ -1,8 +1,5 @@
 package cn.march.analysis.model.akka;
 
-
-
-
 import static java.util.concurrent.TimeUnit.SECONDS;
 import scala.concurrent.duration.Duration;
 import akka.actor.ActorRef;
@@ -15,72 +12,73 @@ import akka.japi.Procedure;
 
 public class LookupActor extends UntypedActor {
 
-  private final String path;
-  private ActorRef calculator = null;
+	private final String path;
+	private ActorRef calculator = null;
 
-  public LookupActor(String path) {
-    this.path = path;
-    sendIdentifyRequest();
-  }
+	public LookupActor(String path) {
+		this.path = path;
+		sendIdentifyRequest();
+	}
 
-  private void sendIdentifyRequest() {
-    getContext().actorSelection(path).tell(new Identify(path), getSelf());
-    getContext()
-        .system()
-        .scheduler()
-        .scheduleOnce(Duration.create(3, SECONDS), getSelf(),
-            ReceiveTimeout.getInstance(), getContext().dispatcher(), getSelf());
-  }
+	private void sendIdentifyRequest() {
+		getContext().actorSelection(path).tell(new Identify(path), getSelf());
+		getContext()
+				.system()
+				.scheduler()
+				.scheduleOnce(Duration.create(3, SECONDS), getSelf(),
+						ReceiveTimeout.getInstance(),
+						getContext().dispatcher(), getSelf());
+	}
 
-  @Override
-  public void onReceive(Object message) throws Exception {
-    if (message instanceof ActorIdentity) {
-      calculator = ((ActorIdentity) message).getRef();
-      if (calculator == null) {
-        System.out.println("Remote actor not available: " + path);
-      } else {
-        getContext().watch(calculator);
-        getContext().become(active, true);
-      }
+	@Override
+	public void onReceive(Object message) throws Exception {
+		if (message instanceof ActorIdentity) {
+			calculator = ((ActorIdentity) message).getRef();
+			if (calculator == null) {
+				System.out.println("Remote actor not available: " + path);
+			} else {
+				getContext().watch(calculator);
+				getContext().become(active, true);
+			}
 
-    } else if (message instanceof ReceiveTimeout) {
-      sendIdentifyRequest();
+		} else if (message instanceof ReceiveTimeout) {
+			sendIdentifyRequest();
 
-    } else {
-      System.out.println("Not ready yet");
+		} else {
+			System.out.println("Not ready yet");
 
-    }
-  }
+		}
+	}
 
-  Procedure<Object> active = new Procedure<Object>() {
-    @Override
-    public void apply(Object message) {
-      if (message instanceof Op.MathOp) {
-        // send message to server actor
-        calculator.tell(message, getSelf());
+	Procedure<Object> active = new Procedure<Object>() {
+		@Override
+		public void apply(Object message) {
+			if (message instanceof Op.MathOp) {
+				// send message to server actor
+				calculator.tell(message, getSelf());
 
-      } else if (message instanceof Op.AddResult) {
-        Op.AddResult result = (Op.AddResult) message;
-        System.out.printf("Add result: %d + %d = %d\n", result.getN1(),
-            result.getN2(), result.getResult());
+			} else if (message instanceof Op.AddResult) {
+				Op.AddResult result = (Op.AddResult) message;
+				System.out.printf("Add result: %d + %d = %d\n", result.getN1(),
+						result.getN2(), result.getResult());
 
-      } else if (message instanceof Op.SubtractResult) {
-        Op.SubtractResult result = (Op.SubtractResult) message;
-        System.out.printf("Sub result: %d - %d = %d\n", result.getN1(),
-            result.getN2(), result.getResult());
+			} else if (message instanceof Op.SubtractResult) {
+				Op.SubtractResult result = (Op.SubtractResult) message;
+				System.out.printf("Sub result: %d - %d = %d\n", result.getN1(),
+						result.getN2(), result.getResult());
 
-      } else if (message instanceof Terminated) {
-        System.out.println("Calculator terminated");
-        sendIdentifyRequest();
-        getContext().unbecome();
+			} else if (message instanceof Terminated) {
+				System.out.println("Calculator terminated");
+				sendIdentifyRequest();
+				getContext().unbecome();
 
-      } else if (message instanceof ReceiveTimeout) {
-        // ignore
+			} else if (message instanceof ReceiveTimeout) {
+				// ignore
 
-      } else {
-        unhandled(message);
-      }
+			} else {
+				unhandled(message);
+			}
 
-    }
-  };
+		}
+	};
 }
--- src/cn/march/analysis/model/akka/LookupApplication.java
+++ src/cn/march/analysis/model/akka/LookupApplication.java
@@ -1,7 +1,5 @@
 package cn.march.analysis.model.akka;
 
-
-
 import static java.util.concurrent.TimeUnit.SECONDS;
 import java.util.Random;
 import scala.concurrent.duration.Duration;
@@ -11,42 +9,57 @@ import akka.actor.Props;
 import com.typesafe.config.ConfigFactory;
 
 public class LookupApplication {
-  public static void main(String[] args) {
-    if (args.length == 0 || args[0].equals("Calculator"))
-      startRemoteCalculatorSystem();
-    if (args.length == 0 || args[0].equals("Lookup"))
-      startRemoteLookupSystem();
-  }
 
-  public static void startRemoteCalculatorSystem() {
-    final ActorSystem system = ActorSystem.create("CalculatorSystem",
-        ConfigFactory.load(("calculator")));
-    system.actorOf(Props.create(CalculatorActor.class), "calculator");
-    System.out.println("Started CalculatorSystem");
-  }
+	public static void main(String[] args) {
 
-  public static void startRemoteLookupSystem() {
+		if (args.length == 0 || args[0].equals("Calculator"))
+			startRemoteCalculatorSystem();
 
-    final ActorSystem system = ActorSystem.create("LookupSystem",
-        ConfigFactory.load("remotelookup"));
-    final String path = "akka.tcp://CalculatorSystem@127.0.0.1:2552/user/calculator";
-    final ActorRef actor = system.actorOf(
-        Props.create(LookupActor.class, path), "lookupActor");
+		if (args.length == 0 || args[0].equals("Lookup"))
+			startRemoteLookupSystem();
 
-    System.out.println("Started LookupSystem");
-    final Random r = new Random();
-    system.scheduler().schedule(Duration.create(1, SECONDS),
-        Duration.create(1, SECONDS), new Runnable() {
-          @Override
-          public void run() {
-            if (r.nextInt(100) % 2 == 0) {
-              actor.tell(new Op.Add(r.nextInt(100), r.nextInt(100)), null);
-            } else {
-              actor.tell(new Op.Subtract(r.nextInt(100), r.nextInt(100)), null);
-            }
+	}
 
-          }
-        }, system.dispatcher());
+	public static void startRemoteCalculatorSystem() {
 
-  }
+		final ActorSystem system = ActorSystem.create("CalculatorSystem",
+				ConfigFactory.load(("calculator")));
+
+		system.actorOf(Props.create(CalculatorActor.class), "calculator");
+
+		System.out.println("Started CalculatorSystem");
+
+	}
+
+	public static void startRemoteLookupSystem() {
+
+		final ActorSystem system = ActorSystem.create("LookupSystem",
+				ConfigFactory.load("remotelookup"));
+
+		final String path = "akka.tcp://CalculatorSystem@127.0.0.1:2552/user/calculator";
+
+		final ActorRef actor = system.actorOf(
+				Props.create(LookupActor.class, path), "lookupActor");
+
+		System.out.println("Started LookupSystem");
+
+		final Random r = new Random();
+
+		system.scheduler().schedule(Duration.create(1, SECONDS),
+
+		Duration.create(1, SECONDS), new Runnable() {
+			@Override
+			public void run() {
+				if (r.nextInt(100) % 2 == 0) {
+					actor.tell(new Op.Add(r.nextInt(100), r.nextInt(100)), null);
+				} else {
+					actor.tell(new Op.Subtract(r.nextInt(100), r.nextInt(100)),
+							null);
+				}
+
+			}
+		}, system.dispatcher());
+
+	}
+
 }
