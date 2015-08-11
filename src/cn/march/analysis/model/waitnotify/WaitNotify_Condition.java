a7e090e9b5b5465a70afe35d7bef0d0458df2039
xiaohuishu <shuxiaohui@foxmail.com>
2015-08-11 09:48:17 +0800

    update cn.march.analysis.model.waitnotify

--- src/cn/march/analysis/model/waitnotify/WaitNotify_Condition.java
+++ src/cn/march/analysis/model/waitnotify/WaitNotify_Condition.java
@@ -10,23 +10,67 @@
 	import java.util.concurrent.locks.ReentrantLock;
 	
 	
-	
+	/**
+	 * 使用Lock中Condition对象控制线程A,B分别输出：
+	 * 
+	 * 		A --> 1 2 3
+	 * 		B --> 4 5 6
+	 * 		C --> 7 8 9
+	 * 
+	 * Condition对象的作用与Object -> wait(),notify()方法的作用相类似
+	 * 
+	 * 但是一个Lock对象可以创建多个Condition对(lock.newCondition());
+	 * 
+	 * 这样有一个好处就是：
+	 * 
+	 * 		Object -> notify() 唤醒的线程对象是随机的,是不可控的,如果唤醒的对象不是现在程序所需要的则对象会继续睡眠
+	 * 		Condition -> singal() 则是可控的,自己可以控制唤醒的具体是哪个对象,是不是自己所需要的;
+	 * 
+	 * 在这个例子中,我们主要实现对value的操作,由于用到lock所以直接将value定义成int
+	 * 
+	 * 基本思路：
+	 * 
+	 * 		创建一个Lock对象：
+	 * 			Lock lock = new ReetrantLock();
+	 * 		创建2个Condition分别对应线程A,B:
+	 * 			Condition aCondition = lock.newCondition();
+	 * 			Condition bCondition = lock.newCondition();	
+	 * 		线程A：
+	 * 			输出 1 2 3
+	 * 			唤醒线程B -> bCondition.singal();
+	 * 			等待线程B执行 -> aCondtion.await();
+	 * 			输出 7 8 9
+	 * 		
+	 * 		线程B：
+	 * 			判断value值是否小于3,若小于则直接等待线程A执行 -> aCondition.await();
+	 * 			输出 4 5 6
+	 * 			唤醒线程A -> aCondition.singal(); 			
+	 * 
+	 * 
+	 * @author antsmarth
+	 *
+	 */
 	public class WaitNotify_Condition {
 	
+		//声明value变量类
 		static class testInt {
 	
 			public int value = 1;
 	
 		}
 	
+		//测试方法
 		public static void main(String[] args) {
 	
+			//锁对象
 			final Lock lock = new ReentrantLock();
+			//Condition对象
 			final Condition aCondition = lock.newCondition();
 			final Condition bCondition = lock.newCondition();
 	
 			final testInt num = new testInt();
 	
+			//线程A
 			Runnable printA = () -> {
 	
 				lock.lock();
@@ -77,6 +121,7 @@
 	
 			};
 	
+			//线程B
 			Runnable printB = () -> {
 	
 				try {
--- src/cn/march/analysis/model/waitnotify/WaitNotify_Object.java
+++ src/cn/march/analysis/model/waitnotify/WaitNotify_Object.java
@@ -8,11 +8,35 @@
 	import java.util.concurrent.atomic.AtomicInteger;
 	
 	
-	
+	/**
+	 * 使用Object中wait,notify方法控制线程A,B分别输出：
+	 * 
+	 * 		A --> 1 2 3
+	 * 		B --> 4 5 6
+	 * 		C --> 7 8 9
+	 * 
+	 * 基本思路：
+	 * 		定义一个实例变量val(AtomicInteger)本身就是线程安全,不需要加锁处理
+	 * 		线程A：
+	 * 			开始输出1 2 3(val初始为1,每输出一次val increase+1)
+	 * 			调用obj.notify()唤醒线程B
+	 * 			若val值小于6则调用obj.wait()让线程A等待
+	 *			之后val值小于9则直接输出val(7 8 9)
+	 *	
+	 *		线程B：
+	 *			若val小于3,则调用obj.wait()让线程B等待A输出
+	 *			之后val若小于6,则输出val(4 5 6)
+	 *			输出完之后,唤醒线程A
+	 * 
+	 * @author antsmarth
+	 *
+	 */
 	public class WaitNotify_Object {
 	
+		//Int原子操作对象
 		private AtomicInteger val = new AtomicInteger(1);
 	
+		//输出val的值,并自增
 		private void printValAndIncrease() {
 	
 			System.out.println(Thread.currentThread().getName() + ", val Value: "
@@ -21,10 +45,12 @@
 	
 		}
 	
+		//测试方法
 		public static void main(String[] args) {
 	
 			WaitNotify_Object obj = new WaitNotify_Object();
 	
+			//线程A
 			Runnable printA = () -> {
 	
 				while (obj.val.get() <= 3)
@@ -65,6 +91,7 @@
 	
 			};
 	
+			//线程B
 			Runnable printB = () -> {
 	
 				while (obj.val.get() < 3) {
