cebb90ae167a6330c465b4ff8b898065b4f55ab7
xiaohuishu <shuxiaohui@foxmail.com>
2015-08-15 11:28:04 +0800

    update cn.march.analysis.model.future.FileSizeCalc

--- src/cn/march/analysis/juc/future/pool/FileSizeCalc.java
+++ src/cn/march/analysis/juc/future/pool/FileSizeCalc.java
@@ -14,9 +14,17 @@
 	import java.util.concurrent.TimeUnit;
 	
 	
-	
+	/**
+	 * 测试CPU密集型加大线程数并不能提高效率;
+	 * 通常建立线程数：
+	 * 		thread_size = CPU核数 * 2;
+	 * 
+	 * @author antsmarth
+	 *
+	 */
 	public class FileSizeCalc {
 	
+		//测试方法
 		public static void main(String[] args) throws Exception {
 	
 			long startMills = System.currentTimeMillis();
@@ -33,9 +41,16 @@
 	
 		}
 	
+		/**
+		 * 一个Bean,存储当前目录的子目录和当前文件大小;
+		 * @author antsmarth
+		 *
+		 */
 		static class SubDirAndSize {
 	
+			//文件大小
 			private final long size;
+			//子目录
 			private final List<File> subDirs;
 	
 			public SubDirAndSize(long size, List<File> subDirs) {
@@ -55,13 +70,30 @@
 	
 		}
 	
+		/**
+		 * 通过传入的文件path路径：
+		 * 		判断path是否是目录：
+		 * 			若是：
+		 * 				遍历此path下的文件和目录：
+		 * 				如果有文件则计算文件大小
+		 * 				如果有子目录则直接加入集合
+		 * 				返回一个之前创建的Bean对象
+		 * 			否则：
+		 * 				返回null的Bean集合;
+		 * 
+		 * @param path
+		 * @return
+		 */
 		private SubDirAndSize getSubDirsAndSize(File path) {
 	
 			long total = 0;
-			List<File> subDirs = new ArrayList<File>();
+			List<File> subDirs = null;
 	
 			if (path.isDirectory()) {
 	
+				subDirs = new ArrayList<File>();
+				
+				//遍历目录
 				File[] subFiles = path.listFiles();
 	
 				if (subFiles != null) {
@@ -82,13 +114,23 @@
 	
 		}
 	
+		/**
+		 * 通过线程池计算path目录下实际的容量
+		 * 
+		 * @param path
+		 * @return
+		 * @throws Exception
+		 */
 		private long getFileSize(File path) throws Exception {
 	
 			long total = 0;
 	
+			//获取CPU核数
 			final int cpuCore = Runtime.getRuntime().availableProcessors();
-			final int threadSize = cpuCore + 1;
+			//创建cpuCore * 2线程数
+			final int threadSize = cpuCore * 2;
 	
+			//创建线程池
 			ExecutorService executors = Executors.newFixedThreadPool(threadSize);
 	
 			List<File> dirs = new ArrayList<File>() {
@@ -108,6 +150,7 @@
 			SubDirAndSize subDir = null;
 	
 			try {
+				
 				while (!dirs.isEmpty()) {
 	
 					List<Future<SubDirAndSize>> parResults = new ArrayList<Future<SubDirAndSize>>();
