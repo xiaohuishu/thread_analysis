8abea02b7a0b6e6ab2895b129e07721271e6d2b0
xiaohuishu <shuxiaohui@foxmail.com>
2015-08-15 11:18:58 +0800

    update cn.march.analysis.model.future.FileSizeCalc

--- src/cn/march/analysis/juc/future/pool/FileSizeCalc.java
+++ src/cn/march/analysis/juc/future/pool/FileSizeCalc.java
@@ -1,125 +1,147 @@
-package cn.march.analysis.juc.future.pool;
+	
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
 
-public class FileSizeCalc {
-
-	public static void main(String[] args) throws Exception {
-		
-		long startMills = System.currentTimeMillis();
-		
-		FileSizeCalc fileCalc = new FileSizeCalc();
-		
-		long total = fileCalc.getFileSize(new File("/home/antsmarth"));
-		
-		long endMills = System.currentTimeMillis();
-		
-		System.out.printf("total dirs : %dMB%n", total/(1024*1024));
-		System.out.printf("consume time : %.3fs%n ", (endMills - startMills)/1.0e3);
-		
-	}
+	package cn.march.analysis.juc.future.pool;
 	
 	
-	static class SubDirAndSize {
-		
-		private final long size;
-		private final List<File> subDirs;
-		
-		public SubDirAndSize(long size, List<File> subDirs) {
-			
-			this.size = size;
-			this.subDirs = subDirs;
-			
+	
+	import java.io.File;
+	import java.util.ArrayList;
+	import java.util.List;
+	import java.util.concurrent.ExecutorService;
+	import java.util.concurrent.Executors;
+	import java.util.concurrent.Future;
+	import java.util.concurrent.TimeUnit;
+	
+	
+	
+	public class FileSizeCalc {
+	
+		public static void main(String[] args) throws Exception {
+	
+			long startMills = System.currentTimeMillis();
+	
+			FileSizeCalc fileCalc = new FileSizeCalc();
+	
+			long total = fileCalc.getFileSize(new File("/home/antsmarth/视频"));
+	
+			long endMills = System.currentTimeMillis();
+	
+			System.out.printf("total dirs : %dMB%n", total / (1024 * 1024));
+			System.out.printf("consume time : %.3fs%n ",
+					(endMills - startMills) / 1.0e3);
+	
 		}
-
-		public long getSize() {
-			return size;
-		}
-
-		public List<File> getSubDirs() {
-			return subDirs;
-		}
-		
-	}
 	
+		static class SubDirAndSize {
 	
-	private SubDirAndSize getSubDirsAndSize(File path) {
-		
-		long total = 0;
-		List<File> subDirs = new ArrayList<File>();
-		
-		if(path.isDirectory()) {
-			
-			File[] subFiles = path.listFiles();
-			
-			if(subFiles != null) {
-				
-				for(File file : subFiles) {
-					
-					if(file.isFile())
-						total += file.length();
-					else
-						subDirs.add(file);
+			private final long size;
+			private final List<File> subDirs;
+	
+			public SubDirAndSize(long size, List<File> subDirs) {
+	
+				this.size = size;
+				this.subDirs = subDirs;
+	
+			}
+	
+			public long getSize() {
+				return size;
+			}
+	
+			public List<File> getSubDirs() {
+				return subDirs;
+			}
+	
+		}
+	
+		private SubDirAndSize getSubDirsAndSize(File path) {
+	
+			long total = 0;
+			List<File> subDirs = new ArrayList<File>();
+	
+			if (path.isDirectory()) {
+	
+				File[] subFiles = path.listFiles();
+	
+				if (subFiles != null) {
+	
+					for (File file : subFiles) {
+	
+						if (file.isFile())
+							total += file.length();
+						else
+							subDirs.add(file);
+					}
+	
 				}
-				
+	
 			}
-			
+	
+			return new SubDirAndSize(total, subDirs);
+	
 		}
-		
-		return new SubDirAndSize(total, subDirs);
-		
-	}
 	
+		private long getFileSize(File path) throws Exception {
 	
-	private long getFileSize(File path) throws Exception {
-		
-		long total = 0;
-		
-		final int cpuCore = Runtime.getRuntime().availableProcessors();
-		final int threadSize = cpuCore + 1;
-		
-		ExecutorService executors = Executors.newFixedThreadPool(threadSize);
-		
-		List<File> dirs = new ArrayList<File>();
-		SubDirAndSize subDir = null;
-		
-		if(!dirs.isEmpty()) {
+			long total = 0;
+	
+			final int cpuCore = Runtime.getRuntime().availableProcessors();
+			final int threadSize = cpuCore + 1;
+	
+			ExecutorService executors = Executors.newFixedThreadPool(threadSize);
+	
+			List<File> dirs = new ArrayList<File>() {
+				/**
+				 * 
+				 */
+				private static final long serialVersionUID = 1L;
+	
+				{
+	
+					add(path);
+	
+				}
 			
-			List<Future<SubDirAndSize>> parResults = new ArrayList<Future<SubDirAndSize>>();
-			
-			for(File file : dirs) {
-				
-				parResults.add(executors.submit(() -> {
-					
-					return getSubDirsAndSize(file);
-					
-				}));
-				
+			};
+	
+			SubDirAndSize subDir = null;
+	
+			try {
+				while (!dirs.isEmpty()) {
+	
+					List<Future<SubDirAndSize>> parResults = new ArrayList<Future<SubDirAndSize>>();
+	
+					for (File file : dirs) {
+	
+						parResults.add(executors.submit(() -> {
+	
+							return getSubDirsAndSize(file);
+	
+						}));
+	
+					}
+	
+					dirs.clear();
+	
+					for (Future<SubDirAndSize> future : parResults) {
+	
+						subDir = future.get(100, TimeUnit.SECONDS);
+	
+						total += subDir.getSize();
+	
+						dirs.addAll(subDir.getSubDirs());
+					}
+	
+				}
+	
+				return total;
+	
+			} finally {
+	
+				executors.shutdown();
+	
 			}
-			
-			dirs.clear();
-			
-			for(Future<SubDirAndSize> future : parResults) {
-				
-				subDir = future.get(100, TimeUnit.SECONDS);
-				
-				total += subDir.getSize();
-				
-				dirs.addAll(subDir.getSubDirs());
-			}
-			
 		}
-		
-		return total;
+	
 	}
-	
-	
-	
-	
-}
